6fc09a6c7d70343a42ad276df9a1ef7b2a057e84
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-15 13:19:38 +0900

    一旦削除。

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root 'top#index'
   
   get 'top' => 'top#index'
-  get 'join' => 'join#index'
-
+  
+  # 仮
+  get 'join' => 'top#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
