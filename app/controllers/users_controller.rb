dd047d6e2cc57baa8990e6d5ca85c6867e35d82c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-15 15:27:57 +0900

    ユーザ登録処理を実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def create    
+    @user = User.new(user_id: params[:user][:user_id], password: params[:user][:password], name: params[:user][:name], detail: params[:user][:detail])
+    if !@user.save
+      redirect_to '/join', :alert => @user.errors.first[1]
+      return
+    end
+
     redirect_to '/top', :notice => "登録しました。"
   end
 end
