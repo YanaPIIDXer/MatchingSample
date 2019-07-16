1d7d7e10345476ed9ffb9a3a7d549c3a9716b0bb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-16 11:52:12 +0900

    年齢範囲検索を実装。

--- app/controllers/user_search_controller.rb
+++ app/controllers/user_search_controller.rb
@@ -3,11 +3,18 @@ class UserSearchController < ApplicationController
   end
 
   def result
+    if params[:age_min].to_i() > params[:age_max].to_i()
+      redirect_to "/user_search", :alert => "年齢の最小値が年齢の最大値を超えています。"
+      return
+    end
+    
     @users = User.where("user_id <> ?", session[:user_id])
 
     if params[:name] != ""
       @users = @users.where("name like ?", "%#{params[:name]}%")
     end
+
+    @users = @users.where("age >= ? and age <= ?", params[:age_min].to_i(), params[:age_max].to_i())
     
     @userCount = @users.length
   end
