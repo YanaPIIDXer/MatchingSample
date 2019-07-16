15e64c5e82ef95202eefa080dfefecca09933313
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-16 17:10:39 +0900

    既読フラグを追加。

--- db/migrate/20190716065053_create_contact_messages.rb
+++ db/migrate/20190716065053_create_contact_messages.rb
@@ -4,6 +4,7 @@ class CreateContactMessages < ActiveRecord::Migration
       t.integer :contact_id
       t.string :user_id
       t.string :message
+      t.boolean :isRead, default: false
 
       t.timestamps null: false
     end
