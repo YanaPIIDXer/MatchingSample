3efa74127d8e0d74e868ca8efcc1a4c09a0362ed
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-16 15:34:08 +0900

    二人が所属するコンタクトのＩＤを取得するメソッドを追加。

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,19 @@
 class Contact < ActiveRecord::Base
 
     def self.collectUsers(user_id)
-        contacts = ContactUser.where("user_id = ?", user_id)
+        contactUsers = ContactUser.where("user_id = ?", user_id)
         users = []
-        contacts.each do |contact|
-            users.push(ContactUser.where("contact_id = ? and user_id <> ?", contact.id, user_id).first)
+        contactUsers.each do |contactUser|
+            users.push(ContactUser.where("contact_id = ? and user_id <> ?", contactUser.id, user_id).first)
         end
         return users
     end
 
+    def self.findContactId(user_id1, user_id2)
+        contactUser = ContactUser.where("user_id = ?",  user_id1)
+        return ContactUser.where("contact_id = ? and user_id = ?", contactUser.contact_id, user_id2).first
+    end
+    
     def self.pairUser(user_id, contact_id)
         contactUser = ContactUser.where("contact_id = ? and user_id <> >", contact_id, user_id).first
         return User.find_by(user_id: contactUser.user_id)
