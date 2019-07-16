3ad8f6a4611441d93fa7ae765d92a1aca8a2beec
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-16 15:28:13 +0900

    ペアになるユーザを検索するメソッド追加。

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,4 +9,9 @@ class Contact < ActiveRecord::Base
         return users
     end
 
+    def self.pairUser(user_id, contact_id)
+        contactUser = ContactUser.where("contact_id = ? and user_id <> >", contact_id, user_id).first
+        return User.find_by(user_id: contactUser.user_id)
+    end
+
 end
