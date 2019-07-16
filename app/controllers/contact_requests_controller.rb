afc303f41ee4c0d736e6e4f3dc5c7618b2ba96b3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-16 14:09:57 +0900

    リクエスト要求を投げてきているユーザを列挙するようにした。

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -3,5 +3,14 @@ class ContactRequestsController < AuthPageController
     if checkAndRedirectToError()
       return
     end
+
+    reqs = ContactRequest.where("target_user_id = ?", session[:user_id])
+    @requests = []
+    reqs.each do |req|
+      user = User.find_by(user_id: req.target_user_id)
+      if user
+        @requests.push(user)
+      end
+    end
   end
 end
