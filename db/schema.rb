dde22b85a2427dd8ccb5cc9a76d95262209e53b4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-22 18:48:31 +0900

    パスワードを暗号化するようにした。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190716065053) do
+ActiveRecord::Schema.define(version: 20190722093250) do
 
   create_table "contact_messages", force: :cascade do |t|
     t.integer  "contact_id", limit: 4
@@ -42,13 +42,13 @@ ActiveRecord::Schema.define(version: 20190716065053) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "user_id",    limit: 255
-    t.string   "password",   limit: 255
-    t.string   "name",       limit: 255
-    t.integer  "age",        limit: 4
-    t.string   "detail",     limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "user_id",         limit: 255
+    t.string   "name",            limit: 255
+    t.integer  "age",             limit: 4
+    t.string   "detail",          limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
